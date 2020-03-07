8586591390d12a7fb6e7b64a96e0ae514b34698b
Terry Nippard <nippardt@sheridancollege.ca>
2020-03-07 18:12:53 -0500

    Added Player, MultiplayerGame, and FirebaseAPI classes

---
+++ Classes/FirebaseAPI.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseAPI.swift
+//  FinalProject
+//
+//  Created by Terry Nippard on 2020-03-05.
+//  Copyright © 2020 Terry Nippard. All rights reserved.
+//
+
+import Foundation
+
+class FirebaseAPI {
+    
+}
